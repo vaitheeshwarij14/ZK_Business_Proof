f0e2b0c9f9a6af4e49a005a1801b04cec90bc347
Vaitheshwari J <154435285+vaitheeshwarij14@users.noreply.github.com>
2024-09-19 23:45:34 +0530

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main className="main-content">
-      <h1>ZK Business Process Engine</h1>
+      <h1>ZK Business Process </h1>
       <p> Powering the Next generation finance for supplychain, insurance and payments</p>
       <div className="cta-buttons">
         <a href="#" className="cta-button">Click</a>
