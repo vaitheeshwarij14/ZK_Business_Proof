95753a7cc43a62d23e78abdb89238de9de366794
Vaitheshwari J <154435285+vaitheeshwarij14@users.noreply.github.com>
2024-09-19 23:41:38 +0530

    ui changes

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -9,22 +9,22 @@ function Technologies() {
       <div className="technology-grid">
         <TechnologyCard
           title="ZK zkbp"
-          description="Allows to prove your game process without revealing your strategy."
+          description="Allows to prove your business process execution  without revealing details."
           icon="🟩"
         >
-          <p>Your game moves</p>
+          <p>Prove your Businss Processes</p>
         </TechnologyCard>
         <TechnologyCard
           title="Recursive proofs"
-          description="Used to accumulate your game process proof while you’re playing."
+          description="Layered , Composed and Recurive Proofs for Business Scalabiltiy."
           icon="🔵"
           isCenter
         >
           <p>Game state proof</p>
         </TechnologyCard>
         <TechnologyCard
-          title="Mina and protokit networks interplay"
-          description="Combines power of ZK blockchain with fast and gasless transactions enabling multiplayer and background sessions."
+          title="MINA and Multi-Chain Mimimal Proof Availability"
+          description=" Scalable Aggregations for Bringing Enterprise scale Proofs  "
           icon="⚪"
         >
           <p>MTNA</p>
